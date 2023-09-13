37c331f2e86be40e2d478f1b87cd6d681b877dd5
Wendy Nam <142412339+Wendy-Nam@users.noreply.github.com>
2023-09-14 00:15:06 +0900

    Update generate_lipsync_data.py : Issue Debug (backslash on path)

--- game/lip-sync-plugin/generate_lipsync_data.py
+++ game/lip-sync-plugin/generate_lipsync_data.py
@@ -5,8 +5,8 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(script_dir)  # Get the parent directory of the script
 
 def generate_lipsync_data(character_dir, audio_file):
-    # Generate lipsync data using Rhubarb
-    rhubarb_path = os.path.join(script_dir, "Rhubarb-Lip-Sync-1.13.0-macOS", "rhubarb")
+    # Generate lipsync data using Rhubarb (Note : Choose the appropriate Rhubarb version for your OS)
+    rhubarb_path = os.path.join(script_dir, "Rhubarb-Lip-Sync-1.13.0-macOS", "rhubarb") # change this to the appropriate path for your OS
     audio_path = os.path.join(parent_dir, "audio", "voice", character_dir, audio_file)
     output_path = os.path.join(script_dir, "lip-sync-data", character_dir, os.path.splitext(audio_file)[0] + ".txt")
     # Create the corresponding directory structure in the output path
@@ -17,9 +17,9 @@ def generate_lipsync_data(character_dir, audio_file):
     print(f"Lipsync data generated for {audio_file}")
 
 # Iterate through characters and voice files
-characters = [d for d in os.listdir(os.path.join(script_dir, os.pardir, "audio/voice")) if os.path.isdir(os.path.join(script_dir, os.pardir, "audio/voice", d))]
+characters = [d for d in os.listdir(os.path.join(script_dir, os.pardir, "audio", "voice")) if os.path.isdir(os.path.join(script_dir, os.pardir, "audio", "voice", d))]
 for character in characters:
-    voice_dir = os.path.join(os.path.join(script_dir, os.pardir, "audio/voice"), character)
+    voice_dir = os.path.join(os.path.join(script_dir, os.pardir, "audio", "voice"), character)
     voice_files = [f for f in os.listdir(voice_dir) if f.endswith(".wav") or f.endswith(".ogg")]
     for voice_file in voice_files:
         generate_lipsync_data(character, voice_file)
